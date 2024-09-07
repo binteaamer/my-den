24e319273d0fd23a1e7f1b64bd09c1e334f63848
abeeha binte aamer <abeehakhan50@gmail.com>
2024-09-07 19:58:22 +0500

    Update q4.c

--- lab3 /q4.c
+++ lab3 /q4.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 int main()
-int totalcost, fuel consumed
-int totaldist=1207
-int fuelf=118
-int fuelr=123
-printf("enter fuel average");
+{
+float totalfuelcost, fuelconsumed, fuelaverage;
+float singletripdist=1207;
+float fuelprice_forward=118.0;
+float fuelprice_return=123.0;
+printf("enter fuel average of the car");
+scanf("%f" , &fuelaverage );
+if (fuelaverage <=0)
+{printf ("please enter a positive average value\n");}
+fuelconsumed = (2 * singletripdist) / fuelaverage ;
+totalfuelcost = (singletripdist / fuelaverage) * fuelprice_forward
+                    + (singletripdist / fuelaverage) * fuelprice_return;
+printf("Total fuel consumed for the round trip: %.2f liters\n", fuelconsumed);
+    printf("Total fuel cost for the round trip: %.2f\n", totalfuelcost);
+}
