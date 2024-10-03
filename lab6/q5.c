a4e91c419d07684d854eddc8e8f88975b27867c0
abeeha binte aamer <abeehakhan50@gmail.com>
2024-10-03 15:49:22 +0500

    Update q5.c

--- lab6/q5.c
+++ lab6/q5.c
@@ -11,3 +11,21 @@ int main (){
 	    
 	 }
 }
+
+
+
+
+#include <stdio.h>
+int main (){
+	char i;
+	for (i=1 ;i<=30;i++)
+	{printf("*");
+		if (i%5==0){
+		   printf("\n");}
+		if (i%5==1 || i)
+		 printf(" ");
+		 
+	   
+	    
+	 }
+}
