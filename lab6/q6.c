6b8b41a48347d5b9988385fc9934c7d4d5a2ca13
abeeha binte aamer <abeehakhan50@gmail.com>
2024-10-10 14:08:36 +0500

    Update q6.c

--- lab6/q6.c
+++ lab6/q6.c
@@ -11,3 +11,22 @@ int main (){
 	    
 	 }
 }
+
+
+
+
+m2
+#include<stdio.h>
+int main () {
+	int i;
+for (i=1;i<=30;i++){
+	if ((i<=5 || i>=26) || (i%5==10));{
+            printf("*");}
+        else if (i%5!=0)
+           {printf(" ");}
+        else {printf("*")}
+	if(i%5==0){
+	printf("\n");}
+	}
+
+}
