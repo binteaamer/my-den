7651b95575c1123556e4d040a5fabec876200881
abeeha binte aamer <abeehakhan50@gmail.com>
2024-10-16 13:54:16 +0500

    Create q7.c

---
+++ lab7/q7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5], i, j, count;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Storing elements
+    for (i = 0; i < 5; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    // Comparing elements to count occurrences
+    for (i = 0; i < 5; i++) {
+        count = 0;
+        for (j = 0; j < 5; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+        if (count > 1) {
+            printf("Element %d has occurred %d times\n", arr[i], count);
+            break; // Added to ensure we only print once for each duplicate element
+        }
+    }
+
+    return 0;
+}
+
+
+
+//tried many times without nesting but not possible 
