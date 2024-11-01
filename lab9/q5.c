a3edb2854e5c173b2588c9dd4a04ef99754891ac
abeeha binte aamer <abeehakhan50@gmail.com>
2024-11-01 09:59:14 +0500

    Create q5.c

---
+++ lab9/q5.c
@@ -0,0 +1,17 @@
+/*Create a function that reverses a given string and returns the reversed string. Use this function
+in the main program to display the reversed string entered by the user.*/
+ #include<stdio.h>
+ #include<string.h>
+ 
+void reverse(char arr[], int n);
+int main(){
+	char str[10];  int i ;
+	printf("enter a string");
+	scnaf("%s", str);
+	reversed = reverese(str, 10);
+}
+reverse(char arr[], int n)
+{
+	strrev(arr);
+	printf("reversed string is ")
+}
