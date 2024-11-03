8a9ccc3a63b47eea3c2c532bd1c106baacc0fc91
abeeha binte aamer <abeehakhan50@gmail.com>
2024-11-03 23:22:14 +0500

    Update q7.c

--- lab7/q7.c
+++ lab7/q7.c
@@ -2,15 +2,10 @@
 
 int main() {
     int arr[5], i, j, count=0;
-    //int n = sizeof(arr) / sizeof(arr[0]);
-
-    // Storing elements
     for (i = 0; i < 5; i++) {
         printf("Enter element %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
-
-    // Comparing elements to count occurrences
     for (i = 0; i < 5; i++) {
         count = 0;
         for (j = 0; j < 5; j++) {
@@ -20,7 +15,7 @@ int main() {
         }
         if (count > 1) {
             printf("Element %d has occurred %d times\n", arr[i], count);
-            break; // Added to ensure we only print once for each duplicate element
+            break; 
         }
     }
 
