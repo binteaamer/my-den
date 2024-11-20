aaa544b38c0976af13973bcb302a914a8cf9ed68
abeeha binte aamer <abeehakhan50@gmail.com>
2024-11-20 18:53:45 +0500

    Create q8.c

---
+++ lab10/q8.c
@@ -0,0 +1,12 @@
+/*Write a recursive function linearSearch that takes an array, its size, the target element
+to search for, and the current index. It checks if the target is at the current index and 
+to search for, and the current index. It checks if the target is at the current index and */
+
+#include<stdio.h>
+void linearSearch( array [], int size, int target, int index);
+int main ()
+{
+  int array[]={1,2,3,4,5,6,7,8,9,10};
+  
+
+}
